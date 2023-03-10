2de8003b15ef0eca546d5e443571670c9cbf30ca
skibididu <125734451+skibididu@users.noreply.github.com>
2023-03-10 09:01:31 +0100

    Update thanatosbot.py

--- thanatosbot.py
+++ thanatosbot.py
@@ -1,6 +1,5 @@
 import time
 import os
-import socket
 msg =  '''
  _____ _   _   ___   _   _   ___ _____ _____ _____ 
 |_   _| | | | / _ \ | \ | | / _ \_   _|  _  /  ___|
@@ -13,22 +12,13 @@ msg =  '''
 os.system("clear")
 print(msg)
 target = input("(target)-->")
-try: 
- wifi = socket.socket("192.168.1.1")
- socket.connect(wifi)
- print("[+]connction stable")
- hosting = socket.socket(target)
- socket.connect(hosting)
- print("[+]host online")
- nmap = "nmap -Pn -T4 -A "+target 
- ssl = "testssl -U "+target
- cyper = "testssl "+target
- nikto = "nikto --host "+target
- host = "host "+target
- os.system(nmap)
- os.system(ssl)
- os.system(cyper)
- os.system(nikto)
- os.system(host)
-except:
- print("[-]connction error")
+nmap = "nmap -Pn -T4 -A "+target 
+ssl = "testssl -U "+target
+cyper = "testssl "+target
+nikto = "nikto --host "+target
+host = "host "+target
+os.system(nmap)
+os.system(ssl)
+os.system(cyper)
+os.system(nikto)
+os.system(host)
