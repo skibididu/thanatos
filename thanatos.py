bb4bccb4d461f819e224c462eefa13be4f878638
skibididu <125734451+skibididu@users.noreply.github.com>
2023-05-18 08:04:44 +0200

    Update and rename thanatosbot.py to thanatos.py

---
+++ thanatos.py
@@ -0,0 +1,25 @@
+import os
+msg =  '''
+ _____ _   _   ___   _   _   ___ _____ _____ _____
+|_   _| | | | / _ \ | \ | | / _ \_   _|  _  /  ___|
+  | | | |_| |/ /_\ \|  \| |/ /_\ \| | | | | \ `--.
+  | | |  _  ||  _  || . ` ||  _  || | | | | |`--. \
+  | | | | | || | | || |\  || | | || | \ \_/ /\__/ /
+  \_/ \_| |_/\_| |_/\_| \_/\_| |_/\_/  \___/\____/
+  WEB and NETWORKING bot for hacking
+'''
+os.system("clear")
+print(msg)
+target = input("(target)-->")
+nmap = "nmap -Pn -T4 -A "+target
+ssl = "testssl -U "+target
+cyper = "testssl "+target
+nikto = "nikto --host "+target
+host = "host "+target
+os.system(nmap)
+os.system(ssl)
+os.system(cyper)
+os.system(nikto)
+os.system(host)
+
+
